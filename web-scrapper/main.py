8f1838c9ccad1a351a829aa6518e965a79f0f234
Archi Halder <archihalder23026558@gmail.com>
2023-03-22 23:23:32 +0530

    added phone info

--- web-scrapper/main.py
+++ web-scrapper/main.py
@@ -11,6 +11,9 @@ if product == "Movies":
     mr.movie_info()
     mr.movie_review_generator()
 elif product == "Phones":
-    pr.phone_review_generator()
+    data = pr.intro()
+    if data:
+        pr.phone_info(data)
+        pr.phone_review_generator(data)
 else:
     st.markdown("<h1 style='text-align: center'> Product Buying Assistant </h1>", unsafe_allow_html=True)
--- web-scrapper/phone_review.py
+++ web-scrapper/phone_review.py
@@ -5,14 +5,15 @@ import sentiment_score as ss
 import url
 
 
-def phone_review_generator():
-    st.markdown("<h1 style='text-align: center'> Phone Review Generator</h1>",
-                unsafe_allow_html=True)
+def intro():
+    res = []
+    st.markdown("<h1 style='text-align: center'> Phone Review Generator</h1>", unsafe_allow_html=True)
     phone_brand = st.selectbox("Brand Name: ", ["Select a brand", "Apple", "Samsung", "OnePlus", "Xiaomi", "Motorola"])
 
     if phone_brand == "Select a brand":
         pass
     else:
+        res.append(phone_brand)
         model_name, model_id = [], []
         for key, val in url.brands[phone_brand].items():
             if phone_brand == "Apple":
@@ -33,27 +34,45 @@ def phone_review_generator():
         if model == "Select a model":
             pass
         else:
+            res.append(model)
+            res.append(model_id)
             idx = 0
             for i, v in enumerate(model_name):
                 if v == model:
-                    idx = i-1
+                    idx = i - 1
+            res.append(idx)
 
-            url_name = f"{phone_brand.lower()}-{'-'.join(model)}-price-in-india-{model_id[idx]}"
+    if len(res) == 4:
+        url_name = f"{res[0].lower()}-{'-'.join(res[1])}-price-in-india-{res[2][res[3]]}"
+        return url_name
 
-            phone_review = requests.get(f"https://www.gadgets360.com/{url_name}/user-reviews").text
-            soup = BeautifulSoup(phone_review, 'lxml')
 
-            quote = []
-            all_reviews = soup.find_all('div', class_="_cmttxt _wwrap")
-            for review in all_reviews:
-                quote.append(review.text.strip())
+def phone_info(url_name):
+    if url_name:
+        st.subheader("Phone Info")
+    html = requests.get(f"https://www.gadgets360.com/{url_name}#specs").text
+    soup = BeautifulSoup(html, 'lxml')
+    all_info = soup.find_all('div', class_='_pdsd')
+    for info_data in all_info:
+        title = info_data.find('span', class_='_ttl').text
+        value = info_data.find('span', class_='_vltxt').text
+        st.write(title, ':', value)
 
-            st.subheader("Top 5 reviews")
-            for idx, val in enumerate(quote):
-                if idx == 5:
-                    break
-                st.write(val)
 
-            phone_rating = ss.review_score(quote)
-            ss.display_scores(phone_rating)
+def phone_review_generator(url_name):
+    phone_review = requests.get(f"https://www.gadgets360.com/{url_name}/user-reviews").text
+    soup = BeautifulSoup(phone_review, 'lxml')
 
+    quote = []
+    all_reviews = soup.find_all('div', class_="_cmttxt _wwrap")
+    for review in all_reviews:
+        quote.append(review.text.strip())
+
+    st.subheader("Top 5 reviews")
+    for i, val in enumerate(quote):
+        if i == 5:
+            break
+        st.write(val)
+
+    phone_rating = ss.review_score(quote)
+    ss.display_scores(phone_rating)
